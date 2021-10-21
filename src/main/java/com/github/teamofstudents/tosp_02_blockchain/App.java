87d695fc50bcdc8b69db95fc386246d3278429f0
Holger <86824184+HolgerSin@users.noreply.github.com>
2021-10-21 04:55:25 +0200

    initial Maven sample project

---
+++ src/main/java/com/github/teamofstudents/tosp_02_blockchain/App.java
@@ -0,0 +1,13 @@
+package com.github.teamofstudents.tosp_02_blockchain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
